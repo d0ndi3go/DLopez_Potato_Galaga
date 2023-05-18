07328fc5f30334af2b7d5929555f6cda61679925
d0ndi3go <dzedolc27@gmail.com>
2023-05-17 23:25:12 -0700

    push

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from os import path
 
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "images")
+
 # create game class in order to pass properties to the sprites file
 
 class Game:
